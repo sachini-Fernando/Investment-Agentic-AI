03497f6e00fd55918cf99938a0aa95b8e7e9a66f
SETHMI MANCHANAYAKA <sethmidisara@gmail.com>
2026-09-06 21:17:16 +0530

    feat: initialize data acquisition tools

--- src/tools/data_tools.py
+++ src/tools/data_tools.py
@@ -1,1 +1,14 @@
 # member 1 tools
+""" 
+Data Acquisition & Information Retrieval Tools 
+Provides functions for fetching stock data, financial statements, news 
+articles, and IR search. 
+""" 
+ 
+from typing import Dict, List, Optional, Any 
+from loguru import logger 
+ 
+from ..pipeline import MarketDataIngestion, NewsVectorStore 
+ 
+_INGESTION = MarketDataIngestion() 
+_VECTOR_STORE = NewsVectorStore()
